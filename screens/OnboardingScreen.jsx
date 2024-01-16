3989ea0885d330ab6d2fa136fda2a11a0def734a
profsain <husseinimudiking@gmail.com>
2024-01-16 07:38:20 +0100

    adding onboarding

---
+++ screens/OnboardingScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OnboardingScreen = () => {
+  return (
+    <View>
+      <Text className="text-orange-600 text-3xl">OnboardingScreen</Text>
+    </View>
+  )
+}
+
+export default OnboardingScreen
