ab4fc252db913b07beef06e87f0d218820d96f49
profsain <husseinimudiking@gmail.com>
2024-08-20 14:01:27 +0100

    redire error fix

--- App.js
+++ App.js
@@ -3,12 +3,23 @@ import { config } from "@gluestack-ui/config";
 import registerNNPushToken from 'native-notify';
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./navigation/MainNavigator";
+import * as Linking from 'expo-linking';
 import { LoginProvider } from "./context/LoginProvider";
 // redux store
 import store from "./store";
 import { Provider } from "react-redux";
 
 export default function App() {
+  const linking = {
+    prefixes: ['splinxplanet://'],
+    config: {
+      screens: {
+        PaymentSuccessScreen: 'payment-success',
+        // Add other screens if needed
+      },
+    },
+  };
+
   // https://app.nativenotify.com/in-app token
   // for notifications service
   const token = process.env.NATIVE_NOTIFY_TOKEN;
@@ -18,7 +29,7 @@ export default function App() {
     <Provider store={store}>
       <LoginProvider>
         <GluestackUIProvider config={config}>
-          <NavigationContainer>
+          <NavigationContainer linking={linking}>
             {/* main navigation switcher */}
             <MainNavigator />
           </NavigationContainer>
--- components/profileSettings/PaymentScreen.jsx
+++ components/profileSettings/PaymentScreen.jsx
@@ -9,20 +9,27 @@ const PaymentScreen = ({navigation, route}) => {
   const { userProfile, token } = useLogin
 
   const handleNavigationStateChange = async (navState) => {
+    // const successURL = 'https://mysite.com/payment-success';
     // check how to get and pass navState
-    if (navState.url.includes("myapp://splinxplanet/payment-success")) {
-      // get id
-      const transactionId = paymentLink.split("/").pop();
-      alert("Payment successful with id: " + id);
-      // verify transaction
-      const verifyPayment = await verifyTransaction(transactionId);
-      if (verifyPayment.status === "success") {
-        // update user subscription
-        Alert.alert("Payment Successful", JSON.stringify(verifyPayment));
-        // navigation.navigate("PaymentSuccessScreen");
+    try {
+      if (navState.url.includes('https://mysite.com/payment-success')) {
+        // get id
+        const transactionId = paymentLink.split("/").pop();
+        alert("Payment successful with id: " + id);
+        // verify transaction
+        const verifyPayment = await verifyTransaction(transactionId);
+        if (verifyPayment.status === "success") {
+          // update user subscription
+          Alert.alert("Payment Successful", JSON.stringify(verifyPayment));
+          // navigation.navigate("PaymentSuccessScreen");
+        }
+        navigation.navigate("PaymentSuccessScreen");
       }
-      navigation.navigate("PaymentSuccessScreen");
+    } catch (error) {
+      console.log(error);
     }
+
+    
   };
  
     return (
