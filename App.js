5e65284471e8545ff1fa690a2c6a72c70ae2c6eb
profsain <husseinimudiking@gmail.com>
2024-09-11 08:24:42 +0100

    gradle fix

--- App.js
+++ App.js
@@ -3,15 +3,12 @@ import { config } from "@gluestack-ui/config";
 import registerNNPushToken from 'native-notify';
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./navigation/MainNavigator";
-import * as Linking from 'expo-linking';
 import { LoginProvider } from "./context/LoginProvider";
 // redux store
 import store from "./store";
 import { Provider } from "react-redux";
 
 export default function App() {
-  // https://app.nativenotify.com/in-app token
-  // for notifications service
   const token = process.env.NATIVE_NOTIFY_TOKEN;
   registerNNPushToken(22245, token);
   
@@ -19,7 +16,7 @@ export default function App() {
     <Provider store={store}>
       <LoginProvider>
         <GluestackUIProvider config={config}>
-          <NavigationContainer linking={linking}>
+          <NavigationContainer>
             {/* main navigation switcher */}
             <MainNavigator />
           </NavigationContainer>
