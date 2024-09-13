0282a691498e15e607bc15c29686d4d3048c531f
profsain <husseinimudiking@gmail.com>
2024-09-13 17:20:23 +0100

    fix json

--- config.js
+++ config.js
@@ -3,14 +3,6 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 export const firebaseConfig = {
-  // apiKey: "AIzaSyAgSJ7UMiprch3nz_dvs22zTRmOrmWYBOc",
-  // authDomain: "splinx-auth.firebaseapp.com",
-  // databaseURL: 'https://splinx-auth.firebaseio.com',
-  // projectId: "splinx-auth",
-  // storageBucket: "splinx-auth.appspot.com",
-  // messagingSenderId: "1086758963869",
-  // appId: "1:1086758963869:web:dd1d1801813fd7698c2274",
-  // measurementId: "G-RXMSVJHVT3",
   apiKey: "AIzaSyDyBx7keaAPfVOBXkIclNOyUeV10VUJHHI",
   authDomain: "splinx-app-2a5a9.firebaseapp.com",
   projectId: "splinx-app-2a5a9",
