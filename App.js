bcca46d1813de432b4038f4e82cd51a39a33bfe8
Ayo <bigmoerell@gmail>
2025-02-07 11:09:43 +0100

    fix

--- App.js
+++ App.js
@@ -13,6 +13,11 @@ import { LoginProvider } from "./context/LoginProvider";
 import store from "./store";
 import { Provider } from "react-redux";
 
+// adaptY IAP INTEGRATION
+import { adapty } from 'react-native-adapty';
+
+adapty.activate('public_live_a8XKKXOL.fuL3WwVYXFjXhIBeTtfh');
+
 
 export default function App() {
   const token = process.env.NATIVE_NOTIFY_TOKEN;
