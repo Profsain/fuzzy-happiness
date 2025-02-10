7c4194337eeb9580f03e570c646b8edfcb61630e
Ayo <bigmoerell@gmail>
2025-02-10 08:01:57 +0100

    fix pods error

--- App.js
+++ App.js
@@ -13,19 +13,11 @@ import { LoginProvider } from "./context/LoginProvider";
 import store from "./store";
 import { Provider } from "react-redux";
 
-// adaptY IAP INTEGRATION
-import { adapty } from 'react-native-adapty';
-import { useEffect } from "react";
-
 
 
 
 export default function App() {
-  // initialize adapty
-  useEffect(() => {
-    adapty.activate('public_live_a8XKKXOL.fuL3WwVYXFjXhIBeTtfh');
-  }, []);
-  
+
   const token = process.env.NATIVE_NOTIFY_TOKEN;
   registerNNPushToken(22245, token);
   
