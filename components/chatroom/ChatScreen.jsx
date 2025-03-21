710863f793fc5f157c9deea5ef435d13c0a6af5a
Ayo <bigmoerell@gmail>
2025-03-21 09:27:29 +0100

    fix

--- components/chatroom/ChatScreen.jsx
+++ components/chatroom/ChatScreen.jsx
@@ -389,7 +389,7 @@ const ChatScreen = () => {
         onContentSizeChange={handleContentSizeChange}
       >
 
-        <View>
+        <View className="flex-1">
           {messages.map((item, index) => {
             if (item.messageType === "text") {
               const isSelected = selectedMessages.includes(item._id);
@@ -493,75 +493,75 @@ const ChatScreen = () => {
               );
             }
           })}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              paddingHorizontal: 10,
+              paddingVertical: 10,
+              borderTopWidth: 1,
+              borderTopColor: "#dddddd",
+              marginBottom: showEmojiSelector ? 0 : 25,
+            }}
+            className="mt-48"
+          >
+            <Entypo
+              onPress={handleEmojiPress}
+              style={{ marginRight: 5 }}
+              name="emoji-happy"
+              size={24}
+              color="gray"
+            />
+
+            <TextInput
+              value={message}
+              onChangeText={(text) => setMessage(text)}
+              style={{
+                flex: 1,
+                height: 40,
+                borderWidth: 1,
+                borderColor: "#dddddd",
+                borderRadius: 20,
+                paddingHorizontal: 10,
+              }}
+              placeholder="Type Your message..."
+            />
+
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 7,
+                marginHorizontal: 8,
+              }}
+            >
+              <Entypo
+                onPress={handlePhotoSend}
+                name="camera"
+                size={24}
+                color="gray"
+              />
+
+              <Feather name="mic" size={24} color="gray" />
+            </View>
+
+            <Pressable
+              onPress={() => handleSend("text")}
+              style={{
+                backgroundColor: primeryColor,
+                paddingVertical: 8,
+                paddingHorizontal: 12,
+                borderRadius: 20,
+              }}
+            >
+              <Text style={{ color: "white", fontWeight: "bold" }}>Send</Text>
+            </Pressable>
+          </View>
         </View>
 
 
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          paddingHorizontal: 10,
-          paddingVertical: 10,
-          borderTopWidth: 1,
-          borderTopColor: "#dddddd",
-          marginBottom: showEmojiSelector ? 0 : 25,
-        }}
-      >
-        <Entypo
-          onPress={handleEmojiPress}
-          style={{ marginRight: 5 }}
-          name="emoji-happy"
-          size={24}
-          color="gray"
-        />
-
-        <TextInput
-          value={message}
-          onChangeText={(text) => setMessage(text)}
-          style={{
-            flex: 1,
-            height: 40,
-            borderWidth: 1,
-            borderColor: "#dddddd",
-            borderRadius: 20,
-            paddingHorizontal: 10,
-          }}
-          placeholder="Type Your message..."
-        />
-
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 7,
-            marginHorizontal: 8,
-          }}
-        >
-          <Entypo
-            onPress={handlePhotoSend}
-            name="camera"
-            size={24}
-            color="gray"
-          />
-
-          <Feather name="mic" size={24} color="gray" />
-        </View>
-
-        <Pressable
-          onPress={() => handleSend("text")}
-          style={{
-            backgroundColor: primeryColor,
-            paddingVertical: 8,
-            paddingHorizontal: 12,
-            borderRadius: 20,
-          }}
-        >
-          <Text style={{ color: "white", fontWeight: "bold" }}>Send</Text>
-        </Pressable>
-      </View>
       </ScrollView>
-
-
+      
       {showEmojiSelector && (
         <EmojiPicker
           onEmojiSelected={(emoji) => {
@@ -573,6 +573,8 @@ const ChatScreen = () => {
         />
       )}
     </KeyboardAvoidingView>
+
+
   );
 };
 
