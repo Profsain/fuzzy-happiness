d4351ba6d5448e091071659ea99d2244cd1dab15
Profsain <husseinimudiking@gmail.com>
2025-07-07 08:00:45 +0100

    fix monday

--- index.js
+++ index.js
@@ -11,5 +11,4 @@ import {AppRegistry} from 'react-native';
 import App from './App'; // Ensure the correct path to your main component
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(main, () => App);
-
+AppRegistry.registerComponent(appName, () => App);
